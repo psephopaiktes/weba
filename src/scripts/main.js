4b10b2d5d0afd7b53b3763b0dbc76b539fd3141d
psephopaiktes <hirataakira6@gmail.com>
2017-07-10 22:48:37 +0900

    dz index

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,31 @@ $( function(){
 
     console.log('© hirata 2017');
 
-    //smooth scroll
-    $('#gotop').on('click', function() {
-        $('body,html').animate({scrollTop:0}, 'swing');
-    });
+    // go top
+    // $(window).on('scroll', function() {
+    //     if ($(this).scrollTop() > 200)
+    //         $('#gotop').css('right','0');
+    //     else
+    //         $('#gotop').css('right','-8rem');
+    // });
+    // $('#gotop').on('click', function() {
+    //     $(this).css('right','-8rem');
+    //     $('body,html').animate({scrollTop:0}, 'swing');
+    // });
 
+    // sticky sidebar
+    // $(window).on('scroll', function() {
+        // if ($(this).scrollTop() > 200)
+
+    //smooth scroll
     $('a[href^=#]').click(function(){
-		var speed = 500;
-		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top;
-		$("html, body").animate({scrollTop:position}, speed, "swing");
-		return false;
-	});
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
 
     // fix viewport more than 1920px
     if( $(window).width() > 1920 )
