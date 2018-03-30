e29b22990b8fc9bc1b7035d16623074b2f532171
a-hirata <hirataakira6@gmail.com>
2018-03-31 07:57:45 +0900

    微調整

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -51,26 +51,19 @@ console.log('© hirata 2017');
     var mask = document.getElementById('mask');
     var viewFlag = 0;
 
-    // transform scale は負荷がかかるため読み込み時にメニューがちらつく
-    // いったんdisplay:noneしといて読み込みあとに表示
-    window.onload = function(){
-        menu.box.style.display  = 'block';
-        search.box.style.display  = 'block';
-    };
-
     // target を展開して、hideを隠す関数
     function toggleMenu( target, hide ){
 
         if( target.button.classList.contains('on') ){
             target.button.classList.remove('on');
-            target.box.style.transform  = '';
+            target.box.style.setProperty('--height',0);
             mask.style.display = 'none';
             viewFlag = 0;
         }else{
             target.button.classList.add('on');
             hide.button.classList.remove('on');
-            hide.box.style.transform = '';
-            target.box.style.transform  = 'scaleY(1)';
+            hide.box.style.setProperty('--height',0);
+            target.box.style.setProperty('--height',target.box.scrollHeight+'px');
             mask.style.display = 'block';
             viewFlag = 1;
         }
@@ -95,8 +88,8 @@ console.log('© hirata 2017');
         if( !viewFlag ) return;
         menu.button.classList.remove('on');
         search.button.classList.remove('on');
-        menu.box.style.transform  = '';
-        search.box.style.transform  = '';
+        menu.box.style.setProperty('--height',0);
+        search.box.style.setProperty('--height',0);
         mask.style.display = 'none';
         return;
     }
@@ -106,8 +99,8 @@ console.log('© hirata 2017');
     // 隠れたメニューがPC幅になったときに消えたままなのを防ぐ
     window.addEventListener('resize', function(){
         if( window.innerWidth >= 1280 ){
-            menu.box.style.transform  = '';
-            search.box.style.transform  = '';
+            menu.box.style.setProperty('--height',0);
+            search.box.style.setProperty('--height',0);
         }
     }, false );
 
